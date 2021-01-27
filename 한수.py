62ae12b7294c8a19ba128ead387bc573d2feae00
dbsgh431 <39187226+dbsgh431@users.noreply.github.com>
2021-01-27 16:55:00 +0900

    0127

---
+++ 한수.py
@@ -0,0 +1,17 @@
+N = int(input())
+cnt = 0
+
+res = 0
+while N >= 1:
+    if N > 99:
+        d_list = list(str(N))
+        res = int(d_list[0]) - int(d_list[1])
+        if int(d_list[1]) - int(d_list[2]) == res:
+            cnt += 1
+    else:
+        cnt += 1
+    N -= 1
+
+print(cnt)    
+
+
