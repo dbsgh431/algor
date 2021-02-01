269b3499f578e37abd24d000bb29f20fc9b4b163
dbsgh431 <39187226+dbsgh431@users.noreply.github.com>
2021-02-01 12:06:46 +0900

    210201

--- 그리디 괄호.py
+++ 그리디 괄호.py
@@ -1,4 +1,21 @@
-n = int(input())
-for i in range(1,n+1):
-    print("{0}".format(i))
-        
+S = input().split('-') #-를 기준으로 분리
+num = []
+
+
+for i in S:
+    sum = 0
+    sp = i.split('+') # 모든 경우에서 분리된 문자열 중 '+'를 우선 계산해야 함
+    if len(sp) >= 2: # +로 문자열의 분리 된다면 누적합
+        for i in sp:
+            sum += int(i)
+        num.append(sum)    
+    else:
+        num.append(int(sp[0]))
+
+tmp = num[0]
+
+for i in range(1,len(num)):
+    tmp -= num[i] 
+
+print(tmp)    
+
